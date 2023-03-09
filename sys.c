e74f7f4690756e9dc9804e14b013d7a5b24d342b
Albert Canales Ros <albertcanalesros@gmail.com>
2023-03-09 14:37:23 +0100

    Minor errno fixes

--- sys.c
+++ sys.c
@@ -2,16 +2,12 @@
  * sys.c - Syscalls implementation
  */
 #include <devices.h>
-
 #include <utils.h>
-
 #include <io.h>
-
 #include <mm.h>
-
 #include <mm_address.h>
-
 #include <sched.h>
+#include<errno.h>
 
 #define LECTURA 0
 #define ESCRIPTURA 1
@@ -20,14 +16,14 @@ char buff[4096];
 
 int check_fd(int fd, int permissions)
 {
-  if (fd!=1) return -9; /*EBADF*/
-  if (permissions!=ESCRIPTURA) return -13; /*EACCES*/
+  if (fd!=1) return -EBADF;
+  if (permissions!=ESCRIPTURA) return -EACCES;
   return 0;
 }
 
 int sys_ni_syscall()
 {
-	return -38; /*ENOSYS*/
+	return -ENOSYS;
 }
 
 int sys_getpid()
@@ -53,11 +49,12 @@ int sys_write(int fd, void *buffer, int size) {
 	int ret;
 	ret = check_fd(fd, ESCRIPTURA);
 	if(ret < 0) return ret;
-	if(!buffer) return 14; /*EFAULT*/
-	if(size <= 0 || size > 4096) return 22; /*EINVAL*/
+	if(size < 0) return -EINVAL;
+	ret = access_ok(VERIFY_READ, buff, size);
+	if(ret == 0) return -EFAULT;
 
 	// Copy data
-	if(copy_from_user(buffer, buff, size) < 0) return 28; /*ENOSPC*/
+	if(copy_from_user(buffer, buff, size) < 0) return -ENOSPC;
 
 	// Implement service
 	return sys_write_console(buff, size);
