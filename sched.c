bac8407acd3673f7d6cb1496169244552f9e9303
Toomoch <vallsfustearnau@gmail.com>
2023-03-16 14:52:44 +0100

    init freequeue i readyqueue

--- sched.c
+++ sched.c
@@ -2,9 +2,13 @@
  * sched.c - initializes struct for task 0 anda task 1
  */
 
+#include "list.h"
 #include <sched.h>
 #include <mm.h>
 #include <io.h>
+
+struct list_head freequeue;
+struct list_head readyqueue;
 
 union task_union task[NR_TASKS]
   __attribute__((__section__(".data.task")));
@@ -65,8 +69,17 @@ void init_task1(void)
 
 void init_sched()
 {
+	freequeue.next = &task[0].task.list;
+	for (int i=0; i<NR_TASKS-1; ++i)
+		task[i].task.list.next = &task[i+1].task.list;
+	task[NR_TASKS-1].task.list.next = &freequeue;
 
-}
+
+	freequeue.prev = &task[NR_TASKS-1].task.list;
+	for (int i=NR_TASKS-1; i>0; --i)
+		task[i].task.list.prev = &task[i-1].task.list;
+	task[0].task.list.prev = &freequeue;
+} 
 
 struct task_struct* current()
 {
